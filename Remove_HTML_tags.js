27e7f0261a710c60021138a1125c5374e63a8225
Ishpreet-Singh-45 <ishpreet.singh@infostride.com>
2021-09-16 01:45:25 +0530

    Update

---
+++ Remove_HTML_tags.js
@@ -0,0 +1,18 @@
+
+/**
+ * Remove HTML tags from string
+*/
+
+
+function removeTags(text)
+{
+	// check the string is not null or empty
+	if(text === "" || text === null)
+	{
+		return false
+	}else
+	{
+		// check for paired tags in string
+		const paired_regex = 
+	}
+}
